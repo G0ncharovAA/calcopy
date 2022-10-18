5c9ffd76ffe437b9d6ac6cf3e66a89c02ec29fe8
Sasha_Inverse <incognito@none>
2022-10-18 23:03:27 +0300

    Main looper implemented

--- main.py
+++ main.py
@@ -21,21 +21,46 @@ class OperationType(Enum):
 def main():
     ui.show_greetings()
     while True:
-        operation_type = ui.king_menu()
-        if operation_type.value == OperationType.ADD.value:
-            add_entry()
-        elif operation_type.value == OperationType.FIND.value:
-            find_entry(view.get_name_for_search())
-        elif operation_type.value == OperationType.IMPORT.value:
-            import_data()
-        elif operation_type.value == OperationType.EXPORT.value:
-            export_data()
-        elif operation_type.value == OperationType.EXIT.value:
-            view.show_goodbye()
-            break
+        operation_type = OperationType(int(ui.king_menu()))
+        match operation_type:
+            case OperationType.EXIT:
+                ui.show_goodbye()
+                break
+            case OperationType.DIVIDE:
+                perform_divide()
+            case OperationType.MULTIPLY:
+                perform_multiply()
 
 
-def
+def ask_for_complexity():
+    if ui.ask_for_complex():
+        arg1real, arg1complex = ui.enter_complex_argument()
+        arg2real, arg2complex = ui.enter_complex_argument()
+        arg1 = complex(float(arg1real), float(arg1complex))
+        arg2 = complex(float(arg2real), float(arg2complex))
+    else:
+        arg1 = ui.enter_real_argument()
+        arg2 = ui.enter_real_argument()
+    return arg1, arg2
+
+
+def perform_divide():
+    try:
+        args = ask_for_complexity()
+        result = model_divide.division(args[0], args[1])
+        ui.show_result(result)
+    except Exception as ex:
+        ui.show_error(ex)
+
+
+def perform_multiply():
+    try:
+        args = ask_for_complexity()
+        result = model_mult.multyply(args[0], args[1])
+        ui.show_result(result)
+    except Exception as ex:
+        ui.show_error(ex)
+
 
 if __name__ == '__main__':
     main()
--- user_interface.py
+++ user_interface.py
@@ -1,17 +1,9 @@
-import model_divide  as div
-import model_mult  as mult
-import model_pow  as pow
-import model_sqrt  as sqrt
-import model_subtraction  as sub
-import model_sum  as sum
-
-
 def show_greetings():
     print("Добро пожаловать в CalcoPy!")
 
 
 def king_menu():
-    answer = input("1 - divide (Деление)\n2 - mult (Умножение)\n3 - pow (Возведенее в степень)\n4 - sqrt (Квадратный корень)\n5 - subtraction (Вычетание)\n6 - sum (Слажение)\n0 - выход\nКакую операцию Вам необходимо произвести? :")
+    answer = input("1 - divide (Деление)\n2 - mult (Умножение)\n3 - pow (Возведенее в степень)\n4 - sqrt (Квадратный корень)\n5 - subtraction (Вычетание)\n6 - sum (Сложение)\n0 - выход\nКакую операцию Вам необходимо произвести? :")
     return answer
 
 
@@ -35,3 +27,7 @@ def ask_for_complex():
 
 def show_error(error):
     print(f"Произошла ошибка: {error}")
+
+
+def show_goodbye():
+    print("Вы вышли из программы")
