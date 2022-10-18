83801fcd7d91bd3dbdfe5320e333ea0a0bf2dcae
abmin <AlxAlxGoncharov@gmail.com>
2022-10-18 20:41:42 +0300

    Updated imports

--- main.py
+++ main.py
@@ -18,7 +18,23 @@ class OperationType(Enum):
 
 
 def main():
-    print("Добро пожаловать в CalcoPy!")
+    view.show_greetings()
+    while True:
+        operation_type = view.get_operation_type()
+        # Python настолько крив что не может сравнивать Enum:
+        # operation_type is OperationType.ADD
+        # поэтому приходится сравнивать их порядковые значения(((
+        if operation_type.value == OperationType.ADD.value:
+            add_entry()
+        elif operation_type.value == OperationType.FIND.value:
+            find_entry(view.get_name_for_search())
+        elif operation_type.value == OperationType.IMPORT.value:
+            import_data()
+        elif operation_type.value == OperationType.EXPORT.value:
+            export_data()
+        elif operation_type.value == OperationType.EXIT.value:
+            view.show_goodbye()
+            break
 
 
 if __name__ == '__main__':
