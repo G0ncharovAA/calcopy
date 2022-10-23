32a91bd4525923f869a298eb6231961a5d8e298d
Sasha_Inverse <incognito@none>
2022-10-23 11:02:57 +0300

    chat-bot-started

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from enum import Enum
-
+from secret import KEY
 import user_interface as ui
 import excep
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
 
 class OperationType(Enum):
@@ -16,62 +25,62 @@ class OperationType(Enum):
     SUM = 8
 
 
+args = []
+
+
+def start(update: Update, context: CallbackContext) -> int:
+    ui.tele_print(update = update, context = context, output = ui.show_greetings())
+    ui.tele_print(update=update, context=context, output=ui.king_menu())
+    return 0
+
+
+def on_next(update: Update, context: CallbackContext) -> int:
+    operation_type = excep.check(OperationType, update.message.text)
+    match operation_type:
+        case OperationType.EXIT:
+            ui.tele_print(update=update, context=context, output=ui.show_goodbye())
+            return ConversationHandler.END
+        case OperationType.DIVIDE:
+            pass
+        case OperationType.MULTIPLY:
+            pass
+        case OperationType.POWER:
+            pass
+        case OperationType.SUBTRACTION:
+            pass
+        case OperationType.SUM:
+            pass
+        case OperationType.DIVIDE_INTEGER:
+            pass
+        case OperationType.REMINDER:
+            pass
+        case OperationType.SQRT:
+            pass
+        case _:
+          ui.tele_print(update=update, context=context, output=ui.show_error(operation_type))
+    return 0
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    ui.tele_print(update=update, context=context, output=ui.show_goodbye())
+    return ConversationHandler.END
+
+def __perform_divide():
+    pass
+
 def main():
-    ui.show_greetings()
-    while True:
-        operation_type = OperationType(excep.check_input_main())
-        match operation_type:
-            case OperationType.EXIT:
-                ui.show_goodbye()
-                break
-            case OperationType.DIVIDE:
-                perform_divide()
-            case OperationType.MULTIPLY:
-                perform_multiply()
-            case OperationType.POWER:
-                perform_power()
-            case OperationType.SUBTRACTION:
-                perform_subtraction()
-            case OperationType.SUM:
-                perform_sum()
-            case OperationType.DIVIDE_INTEGER:
-                perform_divide_integer()
-            case OperationType.REMINDER:
-                perform_reminder()
-            case OperationType.SQRT:
-                perform_sqrt()
-
-
-def perform_divide():
-    excep.check_divide()
-
-
-def perform_divide_integer():
-    excep.check_divide_integer()
-
-
-def perform_reminder():
-    excep.check_remainder_division()
-
-
-def perform_multiply():
-    excep.check_mult()
-
-
-def perform_power():
-    excep.check_pow()
-
-
-def perform_sqrt():
-    excep.check_sqrt()
-
-
-def perform_subtraction():
-    excep.check_subtraction()
-
-
-def perform_sum():
-    excep.check_summ()
+    updater = Updater(KEY)
+    dispatcher = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            0: [MessageHandler(Filters.all, on_next)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+    dispatcher.add_handler(conv_handler)
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
